2019ecca4acf05baf7c007e61e652edb3cc17cd4
Kill Bill <tomas.bilevicius@gmail.com>
2022-02-12 22:11:37 +0200

    My site ready for deployment.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+// import './App.css';
+import {Footer, Navbar, Services, Transactions, Welcome } from './components';
+
+const App = () => {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xdB113e89487373bB1c683F4b7E52fab395091E15'
